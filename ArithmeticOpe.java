927338bd894480ee93e388929a3e972db1a8d576
Mandakini-Karande <karandemandakini@gmail.com>
2024-11-06 23:32:24 +0530

    Add files via upload

---
+++ ArithmeticOpe.java
@@ -0,0 +1,29 @@
+package edu;
+
+public class ArithmeticOpe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=10;
+         int b=20;
+    int add,mul,sub,div,mod;
+    add=a+b;
+   System.out.println("addition is=" +add);
+   
+   mul=a*b;
+   System.out.println("multiplication is=" +mul);
+   
+  sub=a-b;
+   System.out.println("substraction is=" +sub);
+   
+   div=a/b;
+   System.out.println("division is=" +div);
+   
+   mod=a%b;
+   System.out.println("remainder is=" +mod);
+ 
+		
+		
+	}
+
+}
