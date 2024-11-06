45cacbf04ccf2d723bffed684a7530f79890f9fa
Mandakini-Karande <karandemandakini@gmail.com>
2024-11-06 23:31:47 +0530

    Add files via upload

---
+++ Area_Rectangle.java
@@ -0,0 +1,17 @@
+package edu;
+
+public class Area_Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		int l=15,b=20;
+		int area;
+		
+		area=l*b;
+		System.out.println("area of rectangle is ="+area);
+		
+		
+	}
+
+}
