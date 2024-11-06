8e1f9f559cf4198d0f3e10787b76342da5e051a3
Mandakini-Karande <karandemandakini@gmail.com>
2024-11-06 23:31:23 +0530

    Add files via upload

---
+++ Area_of_square.java
@@ -0,0 +1,18 @@
+package edu;
+
+public class Area_of_square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		int area;
+		
+		int l=15; 
+		area=l*l;
+		System.out.println("area of square is=" +area);
+		
+    
+		
+	}
+
+}
